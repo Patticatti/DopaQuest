2dfeed45ea11d9f2f5394da2d68d9a71dde3e3a1
Patticatti <xpatticattix@gmail.com>
2024-08-04 20:22:49 -0700

    Gem animation working

--- Assets/Scripts/Task.cs
+++ Assets/Scripts/Task.cs
@@ -7,10 +7,15 @@ using UnityEngine.UI;
 public class Task : MonoBehaviour
 {
     public TaskObject taskObject;
+    public GameObject gemPrefab;  
     public TextMeshProUGUI taskNameText;
-    [SerializeField] TextMeshProUGUI taskRewardText;
-    public Toggle toggle;
     public int taskReward;
+
+    [SerializeField] TextMeshProUGUI taskRewardText;
+    public Transform parentTransform;   // The parent transform for the instantiated gems
+    public Toggle toggle;               // The UI Toggle component
+    public int gemCount = 15;           // Number of gems to instantiate
+    public float spawnInterval = 0.1f; 
 
     private void Start()
     {
@@ -26,7 +31,18 @@ public class Task : MonoBehaviour
         if (isOn)
         {
             GameEventsManager.instance.GemsCollected(taskObject.taskReward); // Add task reward
+            StartCoroutine(SpawnGems());
             // toggle.interactable = false; // Disable the toggle
+        }
+    }
+
+    private IEnumerator SpawnGems()
+    {
+        for (int i = 0; i < gemCount; i++)
+        {
+            GameObject newGem = Instantiate(gemPrefab, parentTransform);
+            newGem.transform.SetParent(parentTransform);
+            yield return new WaitForSeconds(spawnInterval);
         }
     }
 }
